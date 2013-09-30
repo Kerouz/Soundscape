5101b4a85360f4710d0868cf530a9154e59a2789
Luc Le Quiniat <luc.lequiniat@me.com>
2013-09-30 21:29:32 +1000

    Adding core data structure

--- Soundscape/SCDetailViewController.h
+++ Soundscape/SCDetailViewController.h
@@ -7,10 +7,27 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface SCDetailViewController : UIViewController <UISplitViewControllerDelegate>
+@interface SCDetailViewController : UIViewController <UISplitViewControllerDelegate, AVAudioPlayerDelegate>
+{
+    BOOL currentPaused;
+}
 
 @property (strong, nonatomic) id detailItem;
-
 @property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@property (strong, nonatomic) AVAudioPlayer *audioPlayer;
+@property (strong, nonatomic) AVAudioPlayer *audioPlayerLoop;
+
+@property (strong, nonatomic) IBOutlet UISlider *volumeControl;
+@property (strong, nonatomic) IBOutlet UISlider *volumeLoopControl;
+
+- (IBAction)playSound:(id)sender;
+- (IBAction)stopAll:(id)sender;
+- (IBAction)pauseAll:(id)sender;
+- (IBAction)volumeSound:(UISlider *)sender;
+- (IBAction)playLoop1:(id)sender;
+- (IBAction)volumeLoop1:(UISlider *)sender;
+
+
 @end
--- Soundscape/SCDetailViewController.m
+++ Soundscape/SCDetailViewController.m
@@ -11,7 +11,12 @@
 @interface SCDetailViewController ()
 @property (strong, nonatomic) UIPopoverController *masterPopoverController;
 - (void)configureView;
+
+
 @end
+
+NSMutableArray *soundsArray;
+
 
 @implementation SCDetailViewController
 
@@ -43,8 +48,58 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    currentPaused = NO;
+    soundsArray = [NSMutableArray new];
+    
+    
+    
+        NSString *soundFile = [[NSBundle mainBundle] pathForResource:@"Brightside" ofType:@"mp3"];
+        _audioPlayerLoop = [[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL fileURLWithPath:soundFile] error:nil];
+        [soundsArray addObject:_audioPlayerLoop];
+        [_audioPlayerLoop prepareToPlay];
+    
+
+    
+    
+    NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle]
+                                         pathForResource:@"wolves_timber_wolves_howling"
+                                         ofType:@"mp3"]];
+    
+    NSError *error;
+    _audioPlayer = [[AVAudioPlayer alloc]
+                    initWithContentsOfURL:url
+                    error:&error];
+    if (error)
+    {
+        NSLog(@"Error in audioPlayer: %@",
+              [error localizedDescription]);
+    } else {
+        _audioPlayer.delegate = self;
+        [soundsArray addObject:_audioPlayer];
+        [_audioPlayer prepareToPlay];
+    }
+
     [self configureView];
+}
+
+- (void) audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag
+{
+    
+}
+
+- (void) audioPlayerDecodeErrorDidOccur:(AVAudioPlayer *)player error:(NSError *)error
+{
+    
+}
+
+- (void) audioPlayerBeginInterruption:(AVAudioPlayer *)player
+{
+    
+}
+
+- (void) audioPlayerEndInterruption:(AVAudioPlayer *)player withOptions:(NSUInteger)flags
+{
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -52,6 +107,65 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+- (IBAction)playSound:(id)sender
+{
+    //_audioPlayer.numberOfLoops = -1;
+    [_audioPlayer play];
+    
+}
+
+- (IBAction)playLoop1:(id)sender
+{
+    _audioPlayerLoop.numberOfLoops = -1;
+    [_audioPlayerLoop play];
+    
+}
+
+
+
+- (IBAction)stopAll:(id)sender {
+    
+    for (AVAudioPlayer *a in soundsArray) {
+        [a stop];
+//        NSTimeInterval startTime = 0;
+//        [a setCurrentTime:startTime];
+        [a setCurrentTime:0];
+    }
+//    [_audioPlayer stop];
+}
+
+- (IBAction)pauseAll:(id)sender {
+    if (!currentPaused) {
+        for (AVAudioPlayer *a in soundsArray) [a pause];
+        currentPaused = YES;
+    } else {
+        for (AVAudioPlayer *a in soundsArray) [a play];
+        currentPaused = NO;
+    }
+}
+
+
+- (IBAction)volumeSound:(UISlider *)sender
+{
+    if (_audioPlayer != nil)
+    {
+        _audioPlayer.volume = _volumeControl.value;
+    }
+
+}
+
+- (IBAction)volumeLoop1:(UISlider *)sender
+{
+    if (_audioPlayerLoop != nil)
+    {
+        _audioPlayerLoop.volume = _volumeLoopControl.value;
+    }
+    
+}
+
+
 
 #pragma mark - Split view
 
@@ -69,4 +183,9 @@
     self.masterPopoverController = nil;
 }
 
+- (IBAction)PlaySound:(id)sender {
+}
+
+- (IBAction)VolumeSound:(UISlider *)sender {
+}
 @end
